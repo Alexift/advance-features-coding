8fa19d2e586f5882cbcaaeadd5bd88261a4ee725
Alex Iftime <alecs.iftime@gmail.com>
2024-09-25 21:05:47 +0300

    final ex9

---
+++ src/main/java/org/example/ex9_palidrom/Palindorm.java
@@ -0,0 +1,69 @@
+package org.example.ex9_palidrom;
+/*
+Check Palindromic Strings
+Write a program that receives a list of strings from the user.
+Implement a method to check and display whether each string in
+the list is a palindrome (reads the same forwards and backwards) using streams.
+
+req:
+       - o Lista de Stinguri
+
+       Input :
+            -List<String> = {"apa","cana","mama","racecar"}
+
+       Output :
+            -iterativ
+            -Stream
+            --Creati o metoda booleana care verifica ca un string e palindrom.
+ */
+
+
+import java.util.ArrayList;
+
+import java.util.List;
+
+
+public class Palindorm {
+
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>(List.of("apa", "cana", "mama", "racecar"));
+        System.out.println("Elementele sunt palindroame: " + palindromMethodFor(list));
+        System.out.println("Elementele sunt palindroame: " + palindromMethodStream(list));
+    }
+    private static List<String> palindromMethodFor(List<String> list) {
+        List<String> palindroms = new ArrayList<>();
+        for (int i = 0; i < list.size(); i++) {
+            String currentElement = list.get(i);
+            if (isPalindrome2(currentElement)) {
+                palindroms.add(currentElement);
+            }
+        }
+        return palindroms;
+    }
+    private static List<String> palindromMethodStream(List<String> list) {
+        return list.stream()
+                .filter(it -> IsPalindrom(it))
+                .toList();
+    }
+    private static boolean IsPalindrom(String element) {
+        String reversed = new StringBuilder(element).reverse().toString();
+        return element.equals(reversed);
+    }
+    public static boolean isPalindrome2(String str) {
+        int left = 0;
+        int right = str.length() - 1;
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+    // rac e car
+    // i = 0 -> r si j = 6 -> r
+    // i = 1 -> a si j = 5 -> a
+    // i = 2 -> c si j = 4 -> c
+    // i = 3 -> e si j = 3 -> 3
+}
